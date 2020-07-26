ea51b8fc1bf9da764fd02edb1b87fac9994d0976
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-27 00:08:19 +0530

    Update package

--- Sources/Intramodular/UI/FileDirectoryView.swift
+++ Sources/Intramodular/UI/FileDirectoryView.swift
@@ -6,8 +6,102 @@
 
 import Foundation
 import Swift
-import SwiftUI
+import SwiftUIX
 import System
+
+public struct RegularFileRowView: View {
+    public let location: FileLocation
+    
+    public init(location: FileLocation) {
+        self.location = location
+    }
+    
+    public var body: some View {
+        Text(location.url.lastPathComponent)
+    }
+}
+
+public struct FileDirectoryRowView: View {
+    @State var isNavigated: Bool = false
+    
+    public let location: FileLocation
+    
+    public init(location: FileLocation) {
+        self.location = location
+    }
+    
+    public var body: some View {
+        Text(location.url.lastPathComponent)
+            .navigate(isActive: $isNavigated) {
+                FileDirectoryView(root: location)
+            }
+            .onTapGesture {
+                isNavigated = true
+            }
+    }
+}
+
+public struct FileItemRowView: View {
+    @Environment(\.fileManager) var fileManager
+    
+    public let location: FileLocation
+    
+    public init(location: FileLocation) {
+        self.location = location
+    }
+    
+    public var body: some View {
+        Group {
+            if try! FilePath(fileURL: location.url).resolveFileType() == .directory {
+                FileDirectoryRowView(location: location)
+            } else {
+                RegularFileRowView(location: location)
+            }
+        }
+        .contextMenu {
+            TryButton {
+                try fileManager.removeItem(at: location.url)
+            } label: {
+                Text("Delete")
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+public struct FileDirectoryView: View {
+    public let root: [FileLocation]
+    
+    public init(root: [FileLocation]) {
+        self.root = root
+    }
+    
+    public init(root: FileLocation) {
+        self.root = [root]
+    }
+    
+    private var items: [FileLocation] {
+        root.flatMap({ $0.contents ?? [] })
+    }
+    
+    private var title: String {
+        if let first = root.first, root.count == 1 {
+            return first.path.fileName
+        } else {
+            return "Files"
+        }
+    }
+    
+    public var body: some View {
+        List {
+            OutlineGroup(items, id: \.hashValue, children: \.contents) { location in
+                FileItemRowView(location: location)
+            }
+            .navigationBarTitle(Text(title), displayMode: .inline)
+        }
+    }
+}
 
 extension FileLocation {
     fileprivate var contents: [FileLocation]? {
@@ -17,24 +111,6 @@ extension FileLocation {
             .filter({ $0.path.exists })
         
         return (result ?? []).isEmpty ? nil : result
-    }
-}
-
-@available(iOS 14.0, *)
-public struct FileDirectoryView: View {
-    public let root: FileLocation
-    
-    public var body: some View {
-        List(root.contents ?? [], id: \.hashValue, children: \.contents) { location in
-            if try! FilePath(fileURL: location.url).resolveFileType() == .directory {
-                NavigationLink(destination: FileDirectoryView(root: location)) {
-                    Text(location.url.lastPathComponent)
-                }
-            } else {
-                Text(location.url.lastPathComponent)
-            }
-        }
-        .navigationBarTitle(Text(root.path.fileName), displayMode: .inline)
     }
 }
 
--- Sources/Intramodular/UI/FileManagerView.swift
+++ Sources/Intramodular/UI/FileManagerView.swift
@@ -11,36 +11,59 @@ import System
 
 public struct FileManagerView: View {
     public let manager: FileManager
-    public let directory: FileManager.SearchPathDirectory
+    public let directories: [FileManager.SearchPathDirectory]
     public let domainMask: FileManager.SearchPathDomainMask
     
     public init(
         manager: FileManager = .default,
-        directory: FileManager.SearchPathDirectory,
+        directories: [FileManager.SearchPathDirectory],
         domainMask: FileManager.SearchPathDomainMask
     ) {
         self.manager = manager
-        self.directory = directory
+        self.directories = directories
         self.domainMask = domainMask
     }
     
     public var body: some View {
-        NavigationView {
-            List(fileLocations) { location in
-                NavigationLink(destination: FileDirectoryView(root: location)) {
-                    Text(location.url.lastPathComponent)
+        List(locationGroups, id: \.hashValue) { group in
+            group.first.ifSome { root in
+                NavigationLink(destination: FileDirectoryView(root: root)) {
+                    Text(root.url.lastPathComponent)
                 }
             }
-            .navigationBarTitle("Rover")
+        }
+        .navigationBarTitle("Files")
+    }
+    
+    private var locationGroups: [[FileLocation]] {
+        directories.flatMap { directory in
+            FileManager.default
+                .urls(for: directory, in: .allDomainsMask)
+                .map(FileLocation.init(_unsafe:))
+                .sorted(by: { $0.url.lastPathComponent < $1.url.lastPathComponent })
+        }
+        .group(by: { $0.url.lastPathComponent })
+        .sorted(by: { $0.key < $1.key })
+        .map({ $0.value })
+    }
+}
+
+// MARK: - Auxiliary Implementation -
+
+@usableFromInline
+struct FileManagerEnvironmentKey: EnvironmentKey {
+    @usableFromInline
+    static let defaultValue: FileManager = .default
+}
+
+extension EnvironmentValues {
+    public var fileManager: FileManager {
+        get {
+            self[FileManagerEnvironmentKey.self]
+        } set {
+            self[FileManagerEnvironmentKey.self] = newValue
         }
     }
-    
-    private var fileLocations: [FileLocation] {
-        FileManager.default
-            .urls(for: directory, in: domainMask)
-            .map(FileLocation.init(_unsafe:))
-    }
-    
 }
 
 #endif
