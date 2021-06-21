e988d4d35399b7f4fbf6d422d1d229d09bcd99f8
Vatsal Manot <vatsal.manot@yahoo.com>
2021-06-22 00:34:17 +0530

    Update package

--- Sources/Intramodular/SwiftUI/TextFileDocument.swift
+++ Sources/Intramodular/SwiftUI/TextFileDocument.swift
@@ -14,7 +14,7 @@ public struct TextFileDocument: Codable, FileDocument, Hashable, Initiable {
     public init(text: String) {
         self.text = text
     }
-        
+    
     public init(configuration: ReadConfiguration) throws {
         if let data = configuration.file.regularFileContents {
             text = String(decoding: data, as: UTF8.self)
@@ -31,3 +31,35 @@ public struct TextFileDocument: Codable, FileDocument, Hashable, Initiable {
         FileWrapper(regularFileWithContents: try text.data(using: .utf8).unwrap())
     }
 }
+
+#if canImport(PDFKit)
+
+import PDFKit
+
+public struct PDFFileDocument: FileDocument, Hashable, Initiable {
+    public static var readableContentTypes = [UTType.pdf]
+    
+    public let pdf: PDFDocument
+    
+    public init(pdf: PDFDocument) {
+        self.pdf = pdf
+    }
+    
+    public init(configuration: ReadConfiguration) throws {
+        if let data = configuration.file.regularFileContents {
+            pdf = try PDFDocument(data: data).unwrap()
+        } else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+    }
+    
+    public init() {
+        self.init(pdf: PDFDocument())
+    }
+    
+    public func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        FileWrapper(regularFileWithContents: try pdf.dataRepresentation().unwrap())
+    }
+}
+
+#endif
